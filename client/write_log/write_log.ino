8a7c2fd9f2cd1b802d12a39041e1b6d5dcc7da0f
zmdesigns <zackmdesigns@gmail.com>
2019-09-28 02:17:45 -0700

    client-display Added objects/callbacks for touchscreen display buttons Added display interface design file/compiled file for Nextion Enhanced Display Added placeholder png files for display interface

--- client/write_log/write_log.ino
+++ client/write_log/write_log.ino
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <WiFi101.h>
 #include <Arduino_JSON.h>
+#include "Nextion.h"
 /* Arduino defines max and min which collides with std. let's ignore arduino's def */
 #undef max
 #undef min
@@ -13,12 +14,74 @@ char pass[] = SECRET_PASS;
 int keyIndex = 0;   //only for WEP
 bool stopped = false; //for loop to know if client has been stopped
 int status = WL_IDLE_STATUS;
+std::string numpad_value = "";
 Workstation* wrkstn;
-JTServer* server;
+
+//Display
+NexButton bClockIn = NexButton(0, 3, "bClockIn"); //page-id,component-id,component-name
+NexButton bNum1 = NexButton(1, 2, "bNum1");
+NexButton bNum2 = NexButton(1, 3, "bNum2");
+NexButton bNum3 = NexButton(1, 4, "bNum3");
+NexButton bNum4 = NexButton(1, 5, "bNum4");
+NexButton bNum5 = NexButton(1, 6, "bNum5");
+NexButton bNum6 = NexButton(1, 7, "bNum6");
+NexButton bNum7 = NexButton(1, 8, "bNum7");
+NexButton bNum8 = NexButton(1, 9, "bNum8");
+NexButton bNum9 = NexButton(1, 10, "bNum9");
+
+NexText tNumpad = NexText(1, 13, "tNumpad");
+
+NexTouch *nex_listen_list[] = { &bClockIn, 
+                                &bNum1,
+                                &bNum2,
+                                &bNum3,
+                                &bNum4,
+                                &bNum5,
+                                &bNum6,
+                                &bNum7,
+                                &bNum8,
+                                &bNum9,
+                                NULL };
+
+void bClockInPopCallback(void *ptr) {
+    Serial.println("Clock-in button pressed!");
+    bClockIn.setText("Clocked!");
+}
+
+void bNum1PopCallback(void *ptr) { update_numpad_text('1'); }
+void bNum2PopCallback(void *ptr) { update_numpad_text('2'); }
+void bNum3PopCallback(void *ptr) { update_numpad_text('3'); }
+void bNum4PopCallback(void *ptr) { update_numpad_text('4'); }
+void bNum5PopCallback(void *ptr) { update_numpad_text('5'); }
+void bNum6PopCallback(void *ptr) { update_numpad_text('6'); }
+void bNum7PopCallback(void *ptr) { update_numpad_text('7'); }
+void bNum8PopCallback(void *ptr) { update_numpad_text('8'); }
+void bNum9PopCallback(void *ptr) { update_numpad_text('9'); }
+
+void update_numpad_text(char c) {
+    if (numpad_value.length() < 6) {
+        numpad_value += c;
+        tNumpad.setText(numpad_value.c_str());
+    }
+}
 
 void setup() {
     //Initialize serial and wait for port to open
     Serial.begin(9600);
+    //initialize display
+    nexInit();
+    // Register the pop event callback function of the components
+    bClockIn.attachPop(bClockInPopCallback, &bClockIn);
+    bNum1.attachPop(bNum1PopCallback, &bNum1);
+    bNum2.attachPop(bNum2PopCallback, &bNum2);
+    bNum3.attachPop(bNum3PopCallback, &bNum3);
+    bNum4.attachPop(bNum4PopCallback, &bNum4);
+    bNum5.attachPop(bNum5PopCallback, &bNum5);
+    bNum6.attachPop(bNum6PopCallback, &bNum6);
+    bNum7.attachPop(bNum7PopCallback, &bNum7);
+    bNum8.attachPop(bNum8PopCallback, &bNum8);
+    bNum9.attachPop(bNum9PopCallback, &bNum9);
+
     while (!Serial) {
         ; //wait for serial port to connect. Needed for native USB port only
     }
@@ -54,6 +117,9 @@ void loop() {
 
     //handle response from server
     wrkstn->recv_data();
+
+    //handle touch events for display
+    nexLoop(nex_listen_list);
 }
 
 char recv_serial_input() {
