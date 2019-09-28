2e6fb0bc9d08483ac18da5ebc3de6220f2cd87d3
zmdesigns <zackmdesigns@gmail.com>
2019-09-28 10:53:37 -0700

    client-display Moved all display setup/callbacks to display.h to declutter write_log.ino

---
+++ client/write_log/display.h
@@ -0,0 +1,72 @@
+#include <string>
+#include "Nextion.h"
+
+
+//component objects
+// start screen
+NexButton bClockIn = NexButton(0, 3, "bClockIn"); //page-id,component-id,component-name
+
+// numpad screen
+NexButton bNum1 = NexButton(1, 2, "bNum1");
+NexButton bNum2 = NexButton(1, 3, "bNum2");
+NexButton bNum3 = NexButton(1, 4, "bNum3");
+NexButton bNum4 = NexButton(1, 5, "bNum4");
+NexButton bNum5 = NexButton(1, 6, "bNum5");
+NexButton bNum6 = NexButton(1, 7, "bNum6");
+NexButton bNum7 = NexButton(1, 8, "bNum7");
+NexButton bNum8 = NexButton(1, 9, "bNum8");
+NexButton bNum9 = NexButton(1, 10, "bNum9");
+NexText tNumpad = NexText(1, 13, "tNumpad");
+
+
+std::string numpad_value = "";
+void update_numpad_text(char c) {
+    if (numpad_value.length() < 6) {
+        numpad_value += c;
+        tNumpad.setText(numpad_value.c_str());
+    }
+}
+
+//component callbacks
+void bClockInPopCallback(void *ptr) { Serial.println("Clock-in button pressed!"); }
+
+void bNum1PopCallback(void *ptr) { update_numpad_text('1'); }
+void bNum2PopCallback(void *ptr) { update_numpad_text('2'); }
+void bNum3PopCallback(void *ptr) { update_numpad_text('3'); }
+void bNum4PopCallback(void *ptr) { update_numpad_text('4'); }
+void bNum5PopCallback(void *ptr) { update_numpad_text('5'); }
+void bNum6PopCallback(void *ptr) { update_numpad_text('6'); }
+void bNum7PopCallback(void *ptr) { update_numpad_text('7'); }
+void bNum8PopCallback(void *ptr) { update_numpad_text('8'); }
+void bNum9PopCallback(void *ptr) { update_numpad_text('9'); }
+
+
+void attach_callbacks() {
+    //starting screen
+    bClockIn.attachPop(bClockInPopCallback, &bClockIn);
+
+    //numpad screen
+    bNum1.attachPop(bNum1PopCallback, &bNum1);
+    bNum2.attachPop(bNum2PopCallback, &bNum2);
+    bNum3.attachPop(bNum3PopCallback, &bNum3);
+    bNum4.attachPop(bNum4PopCallback, &bNum4);
+    bNum5.attachPop(bNum5PopCallback, &bNum5);
+    bNum6.attachPop(bNum6PopCallback, &bNum6);
+    bNum7.attachPop(bNum7PopCallback, &bNum7);
+    bNum8.attachPop(bNum8PopCallback, &bNum8);
+    bNum9.attachPop(bNum9PopCallback, &bNum9);
+
+}
+
+//add component objects to event listen array
+NexTouch *nex_listen_list[] = {&bClockIn,
+                               &bNum1,
+                               &bNum2,
+                               &bNum3,
+                               &bNum4,
+                               &bNum5,
+                               &bNum6,
+                               &bNum7,
+                               &bNum8,
+                               &bNum9,
+                               NULL };
