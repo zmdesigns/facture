a5a3ed3919e3b05cef89c32a64e6e6481fe17bee
zmdesigns <zackmdesigns@gmail.com>
2019-11-06 23:07:20 -0800

    webapp-index Made jobs.php -> index.php (it's the main page and makes sense for it to be home for now)

--- webapp/include/nav.php
+++ webapp/include/nav.php
@@ -1,7 +1,7 @@
 <nav>
 <ul>
 <div class='logo-nav'><a href='index.php'><img src="img/web_logo.png" alt="logo"></a></div>
-<li class='jobs-nav'><a href='jobs.php' class='jobs'>Jobs</a></li>
+<li class='jobs-nav'><a href='index.php' class='jobs'>Jobs</a></li>
 <li class='products-nav'><a href='products.php' class='products'>Products</a></li>
 <li class='customers-nav'><a href='customers.php' class='customers'>Customers</a></li>
 <li class='employees-nav'><a href='employees.php' class='employees'>Employees</a></li>
